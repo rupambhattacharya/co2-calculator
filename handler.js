9a0218bc17de62e3252e1fa032a19c01f7ea993a
Rupam <rupam.bhattacharya@burda-forward.de>
2021-06-11 18:39:49 +0200

    add code without tests

--- handler.js
+++ handler.js
@@ -32,22 +32,37 @@ async function getCoordinates(token, loc) {
     return data;
 }
 
-function getDistance(token, lat1, long1, lat2, long2) {
-    request({
-        method: 'GET',
-        url: `https://api.openrouteservice.org/geocode/search?api_key=${token}&text=${start}&layers=locality`,
+async function getDistance(token, lat1, long1, lat2, long2) {
+    const body = JSON.stringify({
+        "locations": [
+            [
+                long1,
+                lat1
+            ],
+            [
+                long2,
+                lat2
+            ],
+        ],
+        "metrics": [
+            "distance"
+        ]
+    });
+
+    const config = {
+        method: 'post',
+        url: 'https://api.openrouteservice.org/v2/matrix/driving-car',
         headers: {
-            'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8'
-        }
-    }, function (error, response, body) {
-        console.log('Status:', response.statusCode);
-        console.log('Headers:', JSON.stringify(response.headers));
-        const coordinate = JSON.parse(response.body).features[0].geometry.coordinates;
-        console.log(coordinate);
-        if (response.statusCode == '200' && coordinate != undefined && coordinate.length != 0) {
-            return coordinate;
-        }
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        data: body
+    };
+    const data = Request(config).then(function (response, error) {
+        const res = response.data.distances[1][0];
+        return res;
     })
+    return data;
 }
 
 function calculateConsumption(distance, mode) {
@@ -56,7 +71,7 @@ function calculateConsumption(distance, mode) {
     for (var i = 0; i < keys.length; i++) {
         if (mode == keys[i]) co2Value = co2Data[mode];
     }
-    const consumption = distance * co2Value / 1000;
+    const consumption = (distance * co2Value / 1000000).toFixed(2);
     return consumption;
 }
 
@@ -64,39 +79,34 @@ const start = process.argv.slice(2);
 const end = process.argv.slice(3);
 const mode = process.argv.slice(4);
 
-//check if the user inputs are valid
-
-async function handler() {
+async function compute() {
     // store token in env variable
     const token = '5b3ce3597851110001cf6248087667c7e9d4475abc3a2ad8c91e8d9b';
+    //check if the user inputs are valid
     //call open route servide api to get lat, long 
     try {
         const coordinate1 = await getCoordinates(token, start);
         const long1 = coordinate1[0];
         const lat1 = coordinate1[1];
-        console.log('coordinate1', lat1, long1);
+        // add check if lat 1 and long 1 are set properly
         const coordinate2 = await getCoordinates(token, end);
         const long2 = coordinate2[0];
         const lat2 = coordinate2[1];
-        console.log('coordinate2', lat2, long2);
+        // add check if lat 2 and long 2 are set properly
         // calculate distance between two cities calling open route service api
-        const distance = getDistance(token, lat1, long1, lat2, long2);
+        const distance = await getDistance(token, lat1, long1, lat2, long2);
+        // add check if distance is a whole number
         const output = calculateConsumption(distance, mode);
-        console.log(`Your trip caused ${output}kg of CO2-equivalent.`)
+        // add check if output is set properly
+        console.log(`Your trip caused ${output} kg of CO2-equivalent.`)
     } catch (e) {
         console.log(e);
     }
-
-
-    
-
-    // add check for edge cases
-    // make code more beautiful
-    //write unit tests
+    // write unit tests
     // serverless for deployment to aws
     // es lint in npm
-    // clean up
+    // clean up repo
     // gitlab yml
     // upate read me
 };
-handler();
+compute();
